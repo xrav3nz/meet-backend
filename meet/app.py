60df91d235ba8e4014527c46b3eb281c029e3a34
Kyle Zhao <kyle_0420@hotmail.com>
2016-02-20 15:52:20 -0500

    initial setup

---
+++ meet/app.py
@@ -0,0 +1,71 @@
+import os
+import logging.handlers
+
+from flask import Flask, render_template
+
+from config import config
+from .extensions import db, admin
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    configure_blueprints(app)
+    configure_extensions(app)
+    configure_errorhandlers(app)
+    configure_logging(app)
+    configure_signals(app)
+
+    return app
+
+def configure_extensions(app):
+
+    # Flask-SQLAlchemy
+    db.init_app(app)
+
+    # Flask-Admin
+    admin.init_app(app)
+
+def configure_blueprints(app):
+    pass
+
+def configure_errorhandlers(app):
+    pass
+
+def configure_logging(app):
+
+    logs_folder = os.path.join(app.root_path, os.pardir, "logs")
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]')
+
+    info_log = os.path.join(logs_folder, app.config['INFO_LOG'])
+
+    info_file_handler = logging.handlers.RotatingFileHandler(
+        info_log,
+        maxBytes=100000,
+        backupCount=10
+    )
+
+    info_file_handler.setLevel(logging.INFO)
+    info_file_handler.setFormatter(formatter)
+    app.logger.addHandler(info_file_handler)
+
+    error_log = os.path.join(logs_folder, app.config['ERROR_LOG'])
+
+    error_file_handler = logging.handlers.RotatingFileHandler(
+        error_log,
+        maxBytes=100000,
+        backupCount=10
+    )
+
+    error_file_handler.setLevel(logging.ERROR)
+    error_file_handler.setFormatter(formatter)
+    app.logger.addHandler(error_file_handler)
+
+def configure_signals(app):
+    pass
